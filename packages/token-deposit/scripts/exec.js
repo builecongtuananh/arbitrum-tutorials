5b94bc3a2f02cfe17e6e21f2a35eab9c0fa8170f
gzeon <95478735+gzeoneth@users.noreply.github.com>
2022-08-11 16:01:41 +0800

    gnosis

--- packages/token-deposit/scripts/exec.js
+++ packages/token-deposit/scripts/exec.js
@@ -3,12 +3,25 @@ const { BigNumber, providers, Wallet } = require('ethers')
 const {
   getL2Network,
   Erc20Bridger,
-  L1ToL2MessageStatus,
 } = require('@arbitrum/sdk')
 const { arbLog, requireEnvVariables } = require('arb-shared-dependencies')
-const { expect } = require('chai')
+const EthersAdapter = require('@gnosis.pm/safe-ethers-lib')['default']
+const Safe = require('@gnosis.pm/safe-core-sdk')['default']
+const {
+  SafeEthersSigner,
+  SafeService,
+} = require('@gnosis.pm/safe-ethers-adapters')
 require('dotenv').config()
-requireEnvVariables(['DEVNET_PRIVKEY', 'L1RPC', 'L2RPC'])
+requireEnvVariables([
+  'DEVNET_PRIVKEY',
+  'L1RPC',
+  'L2RPC',
+  'SAFE_SERVICE_URL',
+  'SAFE_ADDRESS',
+  'TOKEN_ADDRESS',
+  'DEST_ADDRESS',
+  'DEPOSIT_AMT'
+])
 
 /**
  * Set up: instantiate L1 / L2 wallets connected to providers
@@ -21,13 +34,17 @@ const l2Provider = new providers.JsonRpcProvider(process.env.L2RPC)
 const l1Wallet = new Wallet(walletPrivateKey, l1Provider)
 const l2Wallet = new Wallet(walletPrivateKey, l2Provider)
 
-/**
- * Set the amount of token to be transferred to L2
- */
-const tokenDepositAmount = BigNumber.from(50)
+const ethAdapter = new EthersAdapter({ ethers, signer: l1Wallet })
+const service = new SafeService(process.env.SAFE_SERVICE_URL)
 
 const main = async () => {
   await arbLog('Deposit token using Arbitrum SDK')
+
+  const safe = await Safe.create({
+    ethAdapter,
+    safeAddress: process.env.SAFE_ADDRESS,
+  })
+  const safeSigner = new SafeEthersSigner(safe, service, l1Provider)
 
   /**
    * Use l2Network to create an Arbitrum SDK Erc20Bridger instance
@@ -36,117 +53,42 @@ const main = async () => {
   const l2Network = await getL2Network(l2Provider)
   const erc20Bridge = new Erc20Bridger(l2Network)
 
-  /**
-   * For the purpose of our tests, here we deploy an standard ERC20 token (DappToken) to L1
-   * It sends its deployer (us) the initial supply of 1000000000000000
-   */
-  console.log('Deploying the test DappToken to L1:')
+  const erc20Address = process.env.TOKEN_ADDRESS
   const L1DappToken = await (
     await ethers.getContractFactory('DappToken')
   ).connect(l1Wallet)
-  const l1DappToken = await L1DappToken.deploy(1000000000000000)
-  await l1DappToken.deployed()
-  console.log(`DappToken is deployed to L1 at ${l1DappToken.address}`)
-  console.log('Approving:')
-  const erc20Address = l1DappToken.address
-
-  /**
-   * We get the address of L1 Gateway for our DappToken, which later helps us to get the initial token balance of Bridge (before deposit)
-   */
+  const l1DappToken = L1DappToken.attach(erc20Address)
   const expectedL1GatewayAddress = await erc20Bridge.getL1GatewayAddress(
     erc20Address,
     l1Provider
   )
-  const initialBridgeTokenBalance = await l1DappToken.balanceOf(
-    expectedL1GatewayAddress
-  )
+  const tokenAllowance = await l1DappToken.allowance(safeSigner.address, expectedL1GatewayAddress)
+  const tokenDepositAmount = BigNumber.from(process.env.DEPOSIT_AMT)
 
-  /**
-   * The Standard Gateway contract will ultimately be making the token transfer call; thus, that's the contract we need to approve.
-   * erc20Bridge.approveToken handles this approval
-   * Arguments required are:
-   * (1) l1Signer: The L1 address transferring token to L2
-   * (2) erc20L1Address: L1 address of the ERC20 token to be depositted to L2
-   */
-  const approveTx = await erc20Bridge.approveToken({
-    l1Signer: l1Wallet,
-    erc20L1Address: erc20Address,
-  })
+  if (tokenAllowance.lt(tokenDepositAmount)) {
+    const approveTx = await erc20Bridge.approveToken({
+      l1Signer: safeSigner,
+      erc20L1Address: erc20Address,
+    })
+    console.log('sent approve')
+    console.log("USER ACTION REQUIRED")
+    console.log("Go to the Gnosis Safe Web App to confirm the transaction")
+    await approveTx.wait()
+    console.log("Transaction has been executed")
+  }
 
-  const approveRec = await approveTx.wait()
-  console.log(
-    `You successfully allowed the Arbitrum Bridge to spend DappToken ${approveRec.transactionHash}`
-  )
-
-  /**
-   * Deposit DappToken to L2 using erc20Bridge. This will escrow funds in the Gateway contract on L1, and send a message to mint tokens on L2.
-   * The erc20Bridge.deposit method handles computing the necessary fees for automatic-execution of retryable tickets — maxSubmission cost & l2 gas price * gas — and will automatically forward the fees to L2 as callvalue
-   * Also note that since this is the first DappToken deposit onto L2, a standard Arb ERC20 contract will automatically be deployed.
-   * Arguments required are:
-   * (1) amount: The amount of tokens to be transferred to L2
-   * (2) erc20L1Address: L1 address of the ERC20 token to be depositted to L2
-   * (2) l1Signer: The L1 address transferring token to L2
-   * (3) l2Provider: An l2 provider
-   */
   const depositTx = await erc20Bridge.deposit({
     amount: tokenDepositAmount,
     erc20L1Address: erc20Address,
-    l1Signer: l1Wallet,
+    l1Signer: safeSigner,
     l2Provider: l2Provider,
-    destinationAddress: "someaddress"
+    destinationAddress: process.env.DEST_ADDRESS,
   })
-
-  /**
-   * Now we wait for L1 and L2 side of transactions to be confirmed
-   */
-  const depositRec = await depositTx.wait()
-  const l2Result = await depositRec.waitForL2(l2Provider)
-
-  /**
-   * The `complete` boolean tells us if the l1 to l2 message was successul
-   */
-  l2Result.complete
-    ? console.log(
-        `L2 message successful: status: ${L1ToL2MessageStatus[l2Result.status]}`
-      )
-    : console.log(
-        `L2 message failed: status ${L1ToL2MessageStatus[l2Result.status]}`
-      )
-
-  /**
-   * Get the Bridge token balance
-   */
-  const finalBridgeTokenBalance = await l1DappToken.balanceOf(
-    expectedL1GatewayAddress
-  )
-
-  /**
-   * Check if Bridge balance has been updated correctly
-   */
-  expect(
-    initialBridgeTokenBalance
-      .add(tokenDepositAmount)
-      .eq(finalBridgeTokenBalance),
-    'bridge balance not updated after L1 token deposit txn'
-  ).to.be.true
-
-  /**
-   * Check if our l2Wallet DappToken balance has been updated correctly
-   * To do so, we use erc20Bridge to get the l2Token address and contract
-   */
-  const l2TokenAddress = await erc20Bridge.getL2ERC20Address(
-    erc20Address,
-    l1Provider
-  )
-  const l2Token = erc20Bridge.getL2TokenContract(l2Provider, l2TokenAddress)
-
-  const testWalletL2Balance = (
-    await l2Token.functions.balanceOf(l2Wallet.address)
-  )[0]
-  expect(
-    testWalletL2Balance.eq(tokenDepositAmount),
-    'l2 wallet not updated after deposit'
-  ).to.be.true
+  console.log('sent deposit')
+  console.log("USER ACTION REQUIRED")
+  console.log("Go to the Gnosis Safe Web App to confirm the transaction")
+  await depositTx.wait()
+  console.log("Transaction has been executed")
 }
 
 main()
